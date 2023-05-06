75f768596da63629ab9247d2881bcf31a3f8d903
Tai Vo <87288753+vdt2210@users.noreply.github.com>
2023-05-06 20:21:03 +0700

    connect firebase (#14)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-	production: true,
+  firebase: {
+    apiKey: 'AIzaSyByhEI5O3NGlfOBgLoBZPdcuPUip1shyqo',
+    appId: '1:987054504692:web:67192140c98b10826d7ca1',
+    authDomain: 'gps-dashboard-fea13.firebaseapp.com',
+    measurementId: 'G-8C5943S6S4',
+    messagingSenderId: '987054504692',
+    projectId: 'gps-dashboard-fea13',
+    storageBucket: 'gps-dashboard-fea13.appspot.com',
+  },
+  production: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-	production: false,
+  firebase: {
+    apiKey: 'AIzaSyByhEI5O3NGlfOBgLoBZPdcuPUip1shyqo',
+    appId: '1:987054504692:web:67192140c98b10826d7ca1',
+    authDomain: 'gps-dashboard-fea13.firebaseapp.com',
+    measurementId: 'G-8C5943S6S4',
+    messagingSenderId: '987054504692',
+    projectId: 'gps-dashboard-fea13',
+    storageBucket: 'gps-dashboard-fea13.appspot.com',
+  },
+  production: false,
 };
 
 /*
